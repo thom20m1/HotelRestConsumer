f5730be913a2a83737a5198707c882f1341c04d6
Thomas Fogh <thom20m1@edu.easj.dk>
2019-03-07 23:54:51 +0100

    Hotel Get(), GetAll, Delete Implemented

--- HotelRestConsumer/Program.cs
+++ HotelRestConsumer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,18 @@ namespace HotelRestConsumer
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            HotelCode HotelCode = new HotelCode();
+            HotelCode.DoTheStuff();
+            KeepConsoleWindowOpen();
+        }
+        private static void KeepConsoleWindowOpen()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close application");
+            Console.ReadKey();
         }
     }
 }
